adfde60d727789cbecf1ad1f326820a366265d11
Korton TAS <167862450+Korton-TAS-1@users.noreply.github.com>
2024-11-04 14:31:32 +0100

    Update Install-WinGet.ps1
    test

--- Packages/WinGet/Install-WinGet.ps1
+++ Packages/WinGet/Install-WinGet.ps1
@@ -4,6 +4,10 @@ Write-Information "Downloading WinGet and its dependencies..."
 Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
 Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
 Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx -OutFile Microsoft.UI.Xaml.2.8.x64.appx
+Add-AppxProvisionedPackage -Online Microsoft.VCLibs.x64.14.00.Desktop.appx -SkipLicense 
+Add-AppxProvisionedPackage -Online Microsoft.UI.Xaml.2.8.x64.appx -SkipLicense 
+Add-AppxProvisionedPackage -Online Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -SkipLicense 
+
 Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
 Add-AppxPackage Microsoft.UI.Xaml.2.8.x64.appx
 Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
