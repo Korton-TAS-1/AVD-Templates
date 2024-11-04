280e9ac814d0c0a393436801ccab7bdb63d1d29f
Korton TAS <167862450+Korton-TAS-1@users.noreply.github.com>
2024-11-04 10:16:33 +0100

    Update AdobeReader-x64-MUI.ps1
    --accept-source-agreements --accept-source-agreements

--- Packages/Adobe-Reader/AdobeReader-x64-MUI.ps1
+++ Packages/Adobe-Reader/AdobeReader-x64-MUI.ps1
@@ -1,2 +1,2 @@
 #install Adobe Acrobat Reader 64 mui
-winget install -e --id Adobe.Acrobat.Reader.64-bit
+winget install -e --id Adobe.Acrobat.Reader.64-bit --accept-source-agreements --accept-source-agreements
