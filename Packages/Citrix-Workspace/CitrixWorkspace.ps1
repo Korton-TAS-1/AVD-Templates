08a6984b0753c5dbf007ad3592db5fcb0d8a6741
Korton TAS <167862450+Korton-TAS-1@users.noreply.github.com>
2024-11-05 14:49:11 +0100

    Update CitrixWorkspace.ps1

--- Packages/Citrix-Workspace/CitrixWorkspace.ps1
+++ Packages/Citrix-Workspace/CitrixWorkspace.ps1
@@ -1,2 +1,8 @@
-#intsall cirtix workspace
-winget install -e --id Citrix.Workspace --accept-source-agreements --accept-source-agreements
+$ResolveWingetPath = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe\winget.exe"
+    if ($ResolveWingetPath){
+           $WingetPath = $ResolveWingetPath[-1].Path
+    }
+
+$Wingetpath = Split-Path -Path $WingetPath -Parent
+cd $wingetpath
+.\winget.exe install --exact --id Citrix.Workspace --silent --accept-package-agreements --accept-source-agreements
