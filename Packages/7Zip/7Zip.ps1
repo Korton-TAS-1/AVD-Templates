f9d702637e533146e63eacfe2be035c97a4db996
Korton TAS <167862450+Korton-TAS-1@users.noreply.github.com>
2024-11-05 14:47:52 +0100

    Update 7Zip.ps1

--- Packages/7Zip/7Zip.ps1
+++ Packages/7Zip/7Zip.ps1
@@ -1,2 +1,8 @@
-# install 7Zip
-winget install -e --id 7zip.7zip --accept-source-agreements --accept-source-agreements
+$ResolveWingetPath = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe\winget.exe"
+    if ($ResolveWingetPath){
+           $WingetPath = $ResolveWingetPath[-1].Path
+    }
+
+$Wingetpath = Split-Path -Path $WingetPath -Parent
+cd $wingetpath
+.\winget.exe install --exact --id 7zip.7zip --silent --accept-package-agreements --accept-source-agreements
