7edef3574b97298c51af94942d877ae1f7936149
Korton TAS <167862450+Korton-TAS-1@users.noreply.github.com>
2024-11-26 10:35:28 +0100

    Update TimeZone_WE_STD_UTC0100.ps1
    a

--- Scripts/Timezone/TimeZone_WE_STD_UTC0100.ps1
+++ Scripts/Timezone/TimeZone_WE_STD_UTC0100.ps1
@@ -1,4 +1,5 @@
-try{ 
-  cmd.exe /c tzutil /s "W. Europe Standard Time"
-} 
-catch{}
+# Get available time zones by name
+Get-TimeZone -ListAvailable | Where-Object ({$_.ID -like "*Europe*"})
+# Copy the ID of the time zone you get by running Get-TimeZone -ListAvailable | Where-Object ({$_.ID -like "*Europe*"})
+# for instance "W. Europe Standard Time" and use the follwing line to set this time zone for the Azure VM 
+Set-TimeZone -Id "W. Europe Standard Time"
