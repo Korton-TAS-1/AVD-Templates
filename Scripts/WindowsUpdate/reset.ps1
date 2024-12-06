e027e279d3c5920fcf9885029dfc3c647982baae
Korton TAS <167862450+Korton-TAS-1@users.noreply.github.com>
2024-12-06 16:14:22 +0100

    Update reset.ps1

--- Scripts/WindowsUpdate/reset.ps1
+++ Scripts/WindowsUpdate/reset.ps1
@@ -1,3 +1,7 @@
-Stop-Service wuauserv -Force;
+Stop-Service -Name BITS, wuauserv -Force
 Remove-Item "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate" -Recurse -ErrorAction SilentlyContinue -Force
-Start-Service wuauserv;
+Remove-ItemProperty -Name AccountDomainSid, PingID, SusClientId, SusClientIDValidation -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\ -ErrorAction SilentlyContinue
+Remove-Item "$env:SystemRoot\SoftwareDistribution\" -Recurse -Force -ErrorAction SilentlyContinue
+Start-Service -Name BITS, wuauserv
+wuauclt /resetauthorization /detectnow
+(New-Object -ComObject Microsoft.Update.AutoUpdate).DetectNow()
