e42e49d70fb6cc5fc1776e9743d8128a98175087
Korton TAS <167862450+Korton-TAS-1@users.noreply.github.com>
2025-01-09 13:02:15 +0100

    Create AccountView-Optimisation.ps1

---
+++ Scripts/AccountView-Optimisation/AccountView-Optimisation.ps1
@@ -0,0 +1,10 @@
+# https://nl.visma.com/accountview/support/technisch/meldingen/meldingen-in-accountview-door-netwerkstoringen/
+
+# reg key Computer\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\LanmanWorkstation\Parameters\CacheFileTimeout
+# Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\LanmanWorkstation\Parameters' 
+New-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\LanmanWorkstation\Parameters'  -Name CacheFileTimeout -Value 0 -PropertyType DWORD -Force 
+
+# set SmbClientConfiguration 
+Set-SmbClientConfiguration -DirectoryCacheLifetime 0 -Confirm:$false
+Set-SmbClientConfiguration -FileInfoCacheLifetime 0 -Confirm:$false
+Set-SmbClientConfiguration -FileNotFoundCacheLifetime 0 -Confirm:$false
