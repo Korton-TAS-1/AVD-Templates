42bd282de8867a1da0aac568b63e7555d22c8efb
Korton TAS <167862450+Korton-TAS-1@users.noreply.github.com>
2025-04-11 10:37:07 +0200

    Update AccountView-Optimisation.ps1

--- Scripts/AccountView-Optimisation/AccountView-Optimisation.ps1
+++ Scripts/AccountView-Optimisation/AccountView-Optimisation.ps1
@@ -1,5 +1,5 @@
 # https://nl.visma.com/accountview/support/technisch/meldingen/meldingen-in-accountview-door-netwerkstoringen/
-
+Write-Host "Starting AccountView Optimisation: "
 # reg key Computer\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\LanmanWorkstation\Parameters\CacheFileTimeout
 # Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\LanmanWorkstation\Parameters' 
 New-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\LanmanWorkstation\Parameters'  -Name CacheFileTimeout -Value 0 -PropertyType DWORD -Force 
